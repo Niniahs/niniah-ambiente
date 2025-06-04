fd7d6cd34b3e7a3fb3f06185396b77b951de175d
Niniahs <Ivan.santos089@gmail.com>
2025-06-04 12:04:43 -0300

    Update extrair_roas.py

--- extrair_roas.py
+++ extrair_roas.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
+import re
 
 with open('shopee_ads.html', 'r', encoding='utf-8') as f:
     soup = BeautifulSoup(f, 'html.parser')
@@ -7,12 +8,13 @@ nomes_campanha = [tag.get_text(strip=True) for tag in soup.find_all('div', class
 roas_destino = [tag.get_text(strip=True) for tag in soup.find_all('span', class_='roi-value')]
 roas_atual_all = [tag.get_text(strip=True) for tag in soup.find_all('div', class_='report-format-number')]
 
-import re
-# Apenas campanhas que possuem ROAS Destino numérico
 campanhas_reais = []
 for i in range(min(len(nomes_campanha), len(roas_destino), len(roas_atual_all))):
-    # Se o ROAS Destino é um número (exclui banners e propagandas)
-    if re.match(r'^\d+(\.\d+)?$', roas_destino[i].replace(',', '.').strip()):
+    # Limpa o ROAS destino: remove "R$", vírgulas, espaços, aspas, etc.
+    roas_limpo = re.sub(r'[^\d.,]', '', roas_destino[i])
+    roas_limpo = roas_limpo.replace(',', '.').replace('..', '.')
+    # Considera campanha real se o ROAS for número e diferente de vazio
+    if roas_limpo.replace('.', '', 1).isdigit():
         campanhas_reais.append((nomes_campanha[i], roas_destino[i], roas_atual_all[i]))
 
 print("Campanha | ROAS Destino | ROAS Atual")
